000c8729fe9a6399016c734d0cfc5b19fa103e4c
mathom6 <nowwithemus@gmail.com>
2024-10-27 14:49:34 -0400

    success

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,9 +1,9 @@
-SELECT customers.customer_name AS 'Customer', (subscriptions.price_per_month * subscriptions.price_per_month) AS 'Amount Due'
+SELECT customers.customer_name AS 'Customer',  PRINTF('$%.2f', SUM(subscriptions.subscription_length * subscriptions.price_per_month)) AS 'Amount Due'
 FROM orders
 INNER JOIN customers
 ON customers.customer_id = orders.customer_id
 INNER JOIN subscriptions
 ON subscriptions.subscription_id = orders.subscription_id
-WHERE subscriptions.description = 'Fashion Magazine'
-AND orders.order_status = 'unpaid'
+WHERE orders.order_status = 'unpaid'
+AND subscriptions.description = 'Fashion Magazine'
 GROUP BY customers.customer_id;
