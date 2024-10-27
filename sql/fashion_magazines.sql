7f6ecf9a885e8009ae8e0496ab5070a7a592c11b
mathom6 <nowwithemus@gmail.com>
2024-10-27 14:32:07 -0400

    first try

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,9 @@
--- Add your sql here
+SELECT customers.customer_name AS 'Customer', (subscriptions.price_per_month * subscriptions.price_per_month) AS 'Amount Due'
+FROM orders
+INNER JOIN customers
+ON customers.customer_id = orders.customer_id
+INNER JOIN subscriptions
+ON subscriptions.subscription_id = orders.subscription_id
+WHERE subscriptions.description = 'Fashion Magazine'
+AND orders.order_status = 'unpaid'
+GROUP BY customers.customer_id;
